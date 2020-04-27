9f4819085593da98d02c59c8655ad35446d48c87
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-04-27 20:56:53 +0300

    Add documentation and tests for LayerObserver class

--- src/core/layerobserver.cpp
+++ src/core/layerobserver.cpp
@@ -1,18 +1,18 @@
 /***************************************************************************
-                          featuredeltas.h
-                             -------------------
-  begin                : Apr 2020
-  copyright            : (C) 2020 by Ivan Ivanov
-  email                : ivan@opengis.ch
+              featuredeltas.h
+               -------------------
+  begin        : Apr 2020
+  copyright      : (C) 2020 by Ivan Ivanov
+  email        : ivan@opengis.ch
 ***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
+ *                                     *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+ *   the Free Software Foundation; either version 2 of the License, or   *
+ *   (at your option) any later version.                   *
+ *                                     *
  ***************************************************************************/
 
 #include "layerobserver.h"
@@ -22,13 +22,51 @@
 #include <qgsfeaturerequest.h>
 #include <qgsvectorlayereditbuffer.h>
 
+int LayerObserver::sFilesSaved = 0;
 
-// ? should I keep this a general purpose observer or make it LayerDeltaObserver?
+
 LayerObserver::LayerObserver( const QgsProject *project )
-  : mProject( project )
 {
-  mFeatureDeltas = new FeatureDeltas( QStringLiteral( "/home/suricactus/qgis/filename.json" ) );
+  mFeatureDeltas.reset( new FeatureDeltas( generateFileName( project ) ) );
+
   connect( project, &QgsProject::layersAdded, this, &LayerObserver::onLayersAdded );
+}
+
+
+QString LayerObserver::generateFileName( const QgsProject *project )
+{
+  return project->homePath() + QStringLiteral( "/deltafile_%1_%2.json" )
+    .arg( QDateTime::currentDateTime().toMSecsSinceEpoch() )
+    .arg( ++sFilesSaved );
+}
+
+
+QString LayerObserver::fileName() const
+{
+  return mFeatureDeltas->fileName();
+}
+
+
+bool LayerObserver::hasError() const
+{
+  return mFeatureDeltas->hasError();
+}
+
+
+bool LayerObserver::commit()
+{
+  if ( ! mFeatureDeltas->toFile() )
+    return false;
+
+  mFeatureDeltas.reset( new FeatureDeltas( generateFileName( QgsProject::instance() ) ) );
+
+  return true;
+}
+
+
+void LayerObserver::clear() const
+{
+  return mFeatureDeltas->clear();
 }
 
 
@@ -40,7 +78,6 @@ void LayerObserver::onLayersAdded( const QList<QgsMapLayer *> layers )
 
     if ( vl && vl->dataProvider() )
     {
-      // ? what happens when a layer is removed or the project is closed? Are these event listener automatically discarded?
       connect( vl, &QgsVectorLayer::beforeCommitChanges, this, &LayerObserver::onBeforeCommitChanges );
       connect( vl, &QgsVectorLayer::committedFeaturesAdded, this, &LayerObserver::onCommittedFeaturesAdded );
       connect( vl, &QgsVectorLayer::committedFeaturesRemoved, this, &LayerObserver::onCommittedFeaturesRemoved );
@@ -54,125 +91,125 @@ void LayerObserver::onLayersAdded( const QList<QgsMapLayer *> layers )
 
 void LayerObserver::onBeforeCommitChanges()
 {
-    QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
-    QgsVectorLayerEditBuffer *eb = vl->editBuffer();
+  QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
+  QgsVectorLayerEditBuffer *eb = vl->editBuffer();
 
-    if ( ! eb )
-        return;
+  if ( ! eb )
+    return;
 
-    const QgsFeatureIds deletedFids = eb->deletedFeatureIds();
-    // NOTE QgsFeatureIds underlying implementation is QSet, so no need to check if the QgsFeatureId already exists
-    QgsFeatureIds changedFids;
+  const QgsFeatureIds deletedFids = eb->deletedFeatureIds();
+  // NOTE QgsFeatureIds underlying implementation is QSet, so no need to check if the QgsFeatureId already exists
+  QgsFeatureIds changedFids;
 
-    for ( const QgsFeatureId fid : eb->deletedFeatureIds() )
-        changedFids.insert( fid );
+  for ( const QgsFeatureId fid : eb->deletedFeatureIds() )
+    changedFids.insert( fid );
 
-    for ( const QgsFeatureId fid : eb->changedGeometries().keys() )
-        changedFids.insert( fid );
+  for ( const QgsFeatureId fid : eb->changedGeometries().keys() )
+    changedFids.insert( fid );
 
-    for ( const QgsFeatureId fid : eb->changedAttributeValues().keys() )
-        changedFids.insert( fid );
+  for ( const QgsFeatureId fid : eb->changedAttributeValues().keys() )
+    changedFids.insert( fid );
 
-    QgsChangedFeatures changedFeatures;
-    QgsFeatureIterator featuresIt = vl->dataProvider()->getFeatures( QgsFeatureRequest( changedFids ) );
-    QgsFeature f;
+  QgsChangedFeatures changedFeatures;
+  QgsFeatureIterator featuresIt = vl->dataProvider()->getFeatures( QgsFeatureRequest( changedFids ) );
+  QgsFeature f;
 
-    // ? is it possible to use the iterator in a less ugly way? something like normal `for ( QgsFeature &f : it ) {}`
-    while ( featuresIt.nextFeature( f ) )
-        changedFeatures.insert( f.id(), f );
+  // ? is it possible to use the iterator in a less ugly way? something like normal `for ( QgsFeature &f : it ) {}`
+  while ( featuresIt.nextFeature( f ) )
+    changedFeatures.insert( f.id(), f );
 
-    // NOTE no need to keep track of added features, as they are always present in the layer after commit
-    mChangedFeatures.insert( vl->id(), changedFeatures );
-    mPatchedFids.insert( vl->id(), QgsFeatureIds() );
+  // NOTE no need to keep track of added features, as they are always present in the layer after commit
+  mChangedFeatures.insert( vl->id(), changedFeatures );
+  mPatchedFids.insert( vl->id(), QgsFeatureIds() );
 }
 
 
 void LayerObserver::onCommittedFeaturesAdded( const QString &layerId, const QgsFeatureList &addedFeatures )
 {
-    for ( const QgsFeature &newFeature : addedFeatures )
-    {
-        mFeatureDeltas->addCreate( layerId, newFeature );
-    }
+  for ( const QgsFeature &newFeature : addedFeatures )
+  {
+    mFeatureDeltas->addCreate( layerId, newFeature );
+  }
 }
 
 
 void LayerObserver::onCommittedFeaturesRemoved( const QString &layerId, const QgsFeatureIds &deletedFeatureIds )
 {
-    QgsChangedFeatures changedFeatures = mChangedFeatures.value( layerId );
-    
-    for ( const QgsFeatureId &fid : deletedFeatureIds )
-    {
-        Q_ASSERT( changedFeatures.contains( fid ) );
+  QgsChangedFeatures changedFeatures = mChangedFeatures.value( layerId );
+  
+  for ( const QgsFeatureId &fid : deletedFeatureIds )
+  {
+    Q_ASSERT( changedFeatures.contains( fid ) );
 
-        QgsFeature oldFeature = changedFeatures.take( fid );
-        mFeatureDeltas->addDelete( layerId, oldFeature );
-    }
+    QgsFeature oldFeature = changedFeatures.take( fid );
+    mFeatureDeltas->addDelete( layerId, oldFeature );
+  }
 
-    mChangedFeatures.insert( layerId, changedFeatures );
+  mChangedFeatures.insert( layerId, changedFeatures );
 }
 
 
 void LayerObserver::onCommittedAttributeValuesChanges( const QString &layerId, const QgsChangedAttributesMap &changedAttributesValues )
 {
-    QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
-    QgsFeatureIds patchedFids = mPatchedFids.value( layerId );
-    QgsChangedFeatures changedFeatures = mChangedFeatures.value( layerId );
-    
-    for ( const QgsFeatureId &fid : changedAttributesValues.keys() )
-    {
-        Q_ASSERT( changedFeatures.contains( fid ) );
+  QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
+  QgsFeatureIds patchedFids = mPatchedFids.value( layerId );
+  QgsChangedFeatures changedFeatures = mChangedFeatures.value( layerId );
+  
+  for ( const QgsFeatureId &fid : changedAttributesValues.keys() )
+  {
+    Q_ASSERT( changedFeatures.contains( fid ) );
 
-        if ( patchedFids.contains( fid ) )
-          continue;
+    if ( patchedFids.contains( fid ) )
+      continue;
 
-        patchedFids.insert( fid );
+    patchedFids.insert( fid );
 
-        QgsFeature oldFeature = changedFeatures.take( fid );
-        QgsFeature newFeature = vl->getFeature( fid );
-        mFeatureDeltas->addPatch( layerId, oldFeature, newFeature );
-    }
+    QgsFeature oldFeature = changedFeatures.take( fid );
+    QgsFeature newFeature = vl->getFeature( fid );
+    mFeatureDeltas->addPatch( layerId, oldFeature, newFeature );
+  }
 
-    mPatchedFids.insert( layerId, patchedFids );
-    mChangedFeatures.insert( layerId, changedFeatures );
+  mPatchedFids.insert( layerId, patchedFids );
+  mChangedFeatures.insert( layerId, changedFeatures );
 }
 
 
 void LayerObserver::onCommittedGeometriesChanges( const QString &layerId, const QgsGeometryMap &changedGeometries )
 {
-    QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
-    QgsFeatureIds patchedFids = mPatchedFids.value( layerId );
-    QgsChangedFeatures changedFeatures = mChangedFeatures.value( layerId );
+  QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
+  QgsFeatureIds patchedFids = mPatchedFids.value( layerId );
+  QgsChangedFeatures changedFeatures = mChangedFeatures.value( layerId );
 
-    for ( const QgsFeatureId &fid : changedGeometries.keys() )
-    {
-        Q_ASSERT( changedFeatures.contains( fid ) );
+  for ( const QgsFeatureId &fid : changedGeometries.keys() )
+  {
+    Q_ASSERT( changedFeatures.contains( fid ) );
 
-        if ( patchedFids.contains( fid ) )
-          continue;
-        
-        patchedFids.insert(fid);
+    if ( patchedFids.contains( fid ) )
+      continue;
+    
+    patchedFids.insert(fid);
 
-        QgsFeature oldFeature = changedFeatures.take( fid );
-        QgsFeature newFeature = vl->getFeature( fid );
+    QgsFeature oldFeature = changedFeatures.take( fid );
+    QgsFeature newFeature = vl->getFeature( fid );
 
-        mFeatureDeltas->addPatch( layerId, oldFeature, newFeature );
-    }
+    mFeatureDeltas->addPatch( layerId, oldFeature, newFeature );
+  }
 
-    mPatchedFids.insert( layerId, patchedFids );
-    mChangedFeatures.insert( layerId, changedFeatures );
+  mPatchedFids.insert( layerId, patchedFids );
+  mChangedFeatures.insert( layerId, changedFeatures );
 }
 
 
 void LayerObserver::onEditingStopped( )
 {
-    QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
+  QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( sender() );
 
-    mPatchedFids.take( vl->id() );
-    mChangedFeatures.take( vl->id() );
+  mPatchedFids.take( vl->id() );
+  mChangedFeatures.take( vl->id() );
 
-    if ( ! mFeatureDeltas->toFile() )
-    {
-        // TODO somehow indicate the user that writing failed
-        QgsLogger::warning( "Failed writing JSON file" );
-    }
+  if ( ! mFeatureDeltas->toFile() )
+  {
+    // TODO somehow indicate the user that writing failed
+    QgsLogger::warning( "Failed writing JSON file" );
+  }
 }
---
+++ test/test_layerobserver.cpp
@@ -0,0 +1,228 @@
+/***************************************************************************
+                          test_layerobserver.h
+                             -------------------
+  begin                : Apr 2020
+  copyright            : (C) 2020 by Ivan Ivanov
+  email                : ivan@opengis.ch
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <QtTest>
+
+#include "qfield_testbase.h"
+#include "layerobserver.h"
+
+
+class TestLayerObserver: public QObject
+{
+    Q_OBJECT
+  private slots:
+    void initTestCase()
+    {
+      mLayer.reset( new QgsVectorLayer( QStringLiteral( "Point?crs=EPSG:3857&field=int:integer&field=str:string" ), QStringLiteral( "int" ), QStringLiteral( "memory" ) ) );
+      QVERIFY( mLayer->isValid() );
+
+      QgsFeature f1( mLayer->fields() );
+      f1.setAttribute( QStringLiteral( "int" ), 1 );
+      f1.setAttribute( QStringLiteral( "str" ), "string1" );
+      f1.setGeometry( QgsGeometry( new QgsPoint( 25.9657, 43.8356 ) ) );
+      QgsFeature f2( mLayer->fields() );
+      f2.setAttribute( QStringLiteral( "int" ), 2 );
+      f2.setAttribute( QStringLiteral( "str" ), "string2" );
+      f2.setGeometry( QgsGeometry( new QgsPoint( 23.398819, 41.7672147 ) ) );
+      QgsFeature f3( mLayer->fields() );
+      f3.setAttribute( QStringLiteral( "int" ), 3 );
+      f3.setAttribute( QStringLiteral( "str" ), "string3" );
+
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->addFeature( f1 ) );
+      QVERIFY( mLayer->addFeature( f2 ) );
+      QVERIFY( mLayer->addFeature( f3 ) );
+      QVERIFY( mLayer->commitChanges() );
+
+      QgsProject::instance()->setPresetHomePath( QDir::tempPath() );
+      mLayerObserver.reset( new LayerObserver( QgsProject::instance() ) );
+      
+      QVERIFY( QgsProject::instance()->addMapLayer( mLayer.get(), false, false ) );
+      QVERIFY( ! mLayerObserver->hasError() );
+    }
+
+    void init()
+    {
+      mLayerObserver->clear();
+    }
+
+    void cleanup()
+    {
+      mLayerObserver->clear();
+    }
+
+    void testHasError()
+    {
+      // ? how I can test such thing?
+      QSKIP("decide how we test errors");
+      QCOMPARE( mLayerObserver->hasError(), false );
+      QVERIFY( QFile::exists( mLayerObserver->fileName() ) );
+    }
+
+
+    void testFileName()
+    {
+      QVERIFY( mLayerObserver->fileName().size() > 0 );
+      QVERIFY( QFile::exists( mLayerObserver->fileName() ) );
+    }
+
+
+    void testCommit()
+    {
+      QString fileNameOld = mLayerObserver->fileName();
+
+      QVERIFY( mLayerObserver->fileName().size() > 0 );
+      QVERIFY( QFile::exists( mLayerObserver->fileName() ) );
+      QVERIFY( mLayerObserver->commit() );
+  
+      QString fileNameNew = mLayerObserver->fileName();
+  
+      QVERIFY( QFile::exists( fileNameNew ) );
+      QVERIFY( fileNameOld != fileNameNew );
+    }
+
+
+    void testClear()
+    {
+      QgsFeature f1( mLayer->fields() );
+      f1.setAttribute( QStringLiteral( "int" ), 1000 );
+      f1.setAttribute( QStringLiteral( "str" ), "new_string1" );
+      f1.setGeometry( QgsGeometry( new QgsPoint( 25.9657, 43.8356 ) ) );
+
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->addFeature( f1 ) );
+      QVERIFY( mLayer->commitChanges() );
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ).size(), 1 );
+
+      QgsFeature f2( mLayer->fields() );
+      f2.setAttribute( QStringLiteral( "int" ), 1001 );
+      f2.setAttribute( QStringLiteral( "str" ), "new_string2" );
+      f2.setGeometry( QgsGeometry( new QgsPoint( 25.9657, 43.8356 ) ) );
+
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->addFeature( f1 ) );
+      QVERIFY( mLayer->commitChanges() );
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ).size(), 2 );
+
+      mLayerObserver->clear();
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->commitChanges() );
+
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ).size(), 0 );
+    }
+
+
+    void testObservesEditingStopped()
+    {
+      QgsFeature f1( mLayer->fields() );
+      f1.setAttribute( QStringLiteral( "int" ), 1002 );
+      f1.setAttribute( QStringLiteral( "str" ), "new_string" );
+      f1.setGeometry( QgsGeometry( new QgsPoint( 25.9657, 43.8356 ) ) );
+
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->addFeature( f1 ) );
+      // the changes are not written on the disk yet
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ), QStringList() );
+      // when we stop editing, all changes are written
+      QVERIFY( mLayer->commitChanges() );
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ), QStringList({"create"}) );
+    }
+
+
+    void testObservesAdded()
+    {
+      QgsFeature f1( mLayer->fields() );
+      f1.setAttribute( QStringLiteral( "int" ), 1003 );
+      f1.setAttribute( QStringLiteral( "str" ), "new_string" );
+      f1.setGeometry( QgsGeometry( new QgsPoint( 25.9657, 43.8356 ) ) );
+
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->addFeature( f1 ) );
+      QVERIFY( mLayer->commitChanges() );
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ), QStringList({"create"}) );
+    }
+
+
+    void testObservesRemoved()
+    {
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->deleteFeature( 1 ) );
+      QVERIFY( mLayer->commitChanges() );
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ), QStringList({"delete"}) );
+    }
+
+
+    void testObservesAttributeValueChanges()
+    {
+      QgsFeature f1 = mLayer->getFeature( 2 );
+      f1.setAttribute( QStringLiteral( "str" ), f1.attribute( QStringLiteral( "str" ) ).toString() + "_new" );
+      QgsFeature f2 = mLayer->getFeature( 3 );
+      f2.setAttribute( QStringLiteral( "str" ), f2.attribute( QStringLiteral( "str" ) ).toString() + "_new" );
+      f2.setGeometry( QgsGeometry( new QgsPoint( 88.7695313, 51.0897229 ) ) );
+
+      QVERIFY( mLayer->startEditing() );
+      QVERIFY( mLayer->updateFeature( f1 ) );
+      QVERIFY( mLayer->updateFeature( f2 ) );
+      QVERIFY( mLayer->commitChanges() );
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ), QStringList({"patch", "patch"}) );
+    }
+
+
+    void testObservesGeometryChanges()
+    {
+      QVERIFY( mLayer->startEditing() );
+
+      QgsFeature f1 = mLayer->getFeature( 2 );
+      f1.setGeometry( QgsGeometry( new QgsPoint( 13.0545044, 47.8094654 ) ) );
+      QgsFeature f2 = mLayer->getFeature( 3 );
+      f2.setAttribute( QStringLiteral( "str" ), f2.attribute( QStringLiteral( "str" ) ).toString() + "_new" );
+      f2.setGeometry( QgsGeometry( new QgsPoint( 13.0545044, 47.8094654 ) ) );
+
+      QVERIFY( mLayer->updateFeature( f1 ) );
+      QVERIFY( mLayer->updateFeature( f2 ) );
+
+      QVERIFY( mLayer->commitChanges() );
+      QCOMPARE( getDeltaOperations( mLayerObserver->fileName() ), QStringList({"patch", "patch"}) );
+    }
+    
+  private:
+
+    std::unique_ptr<QgsVectorLayer> mLayer;
+    std::unique_ptr<LayerObserver> mLayerObserver;
+
+    QStringList getDeltaOperations( QString fileName )
+    {
+      QStringList operations;
+      QFile deltasFile( fileName );
+
+      if ( ! deltasFile.open( QIODevice::ReadOnly ) )
+        return operations;
+      
+      QJsonDocument doc = QJsonDocument::fromJson( deltasFile.readAll() );
+
+      if ( doc.isNull() )
+        return operations;
+
+      for ( const QJsonValue &v : doc.object().value( "deltas" ).toArray() )
+        operations.append( v.toObject().value( QStringLiteral( "method" ) ).toString() );
+
+      return operations;
+    }
+};
+
+QFIELDTEST_MAIN( TestLayerObserver )
+#include "test_layerobserver.moc"
