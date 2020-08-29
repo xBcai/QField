218f9ee7f9bfacfa3ff32c273b497e7e88060d65
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-08-29 10:38:11 +0300

    Use CI_BUILD_DIR instead

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-source ${DIR}/../version_number.sh
+source ${CI_BUILD_DIR}/../version_number.sh
 
-lupdate -recursive ${DIR} -ts $(ls ${DIR}/i18n/*.ts)
+lupdate -recursive ${CI_BUILD_DIR} -ts $(ls ${CI_BUILD_DIR}/i18n/*.ts)
 
 # release only if the branch is master
 if [[ ${CI_BRANCH} = master ]]; then
   tx push --source
 fi
 
-# release only if the branch is master
+# release only if the branch is revive_translations
 if [[ ${CI_BRANCH} = revive_translations ]]; then
   tx push --source
 fi
