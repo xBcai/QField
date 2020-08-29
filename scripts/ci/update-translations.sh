e85e0b7b2976eb41e2548466e69b522d010dcb42
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-08-29 11:14:01 +0300

    Show me your body

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -5,14 +5,19 @@ source ${CI_BUILD_DIR}/scripts/version_number.sh
 lupdate -recursive ${CI_BUILD_DIR} -ts ${CI_BUILD_DIR}/i18n/qfield_en.ts
 lupdate -recursive ${CI_BUILD_DIR} -ts ${CI_BUILD_DIR}/i18n/qfield_bg.ts
 
+
+echo ==================0
+cat ${CI_BUILD_DIR}/i18n/qfield_en.ts
+echo ==================0
+
 echo ==================1
 grep Changelog ${CI_BUILD_DIR}/i18n/qfield_en.ts
 echo ==================1
 
-
 echo ==================2
 grep Changelog ${CI_BUILD_DIR}/i18n/qfield_bg.ts
 echo ==================2
+
 
 # release only if the branch is master
 if [[ ${CI_BRANCH} = master ]]; then
