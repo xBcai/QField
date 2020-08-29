ef4709e1d4abc2e8f90cee037f097961b0cb5858
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-08-29 10:25:07 +0300

    Export translations for all languages, not only English

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -3,9 +3,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../version_number.sh
 
-lupdate -recursive ${DIR} -ts ${DIR}/i18n/qfield_en.ts
+lupdate -recursive ${DIR} -ts $(ls ${DIR}/i18n/*.ts)
 
 # release only if the branch is master
 if [[ ${CI_BRANCH} = master ]]; then
+  tx push --source
+fi
+
+# release only if the branch is master
+if [[ ${CI_BRANCH} = revive_translations ]]; then
   tx push --source
 fi
